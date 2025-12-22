10b1612c2ede46eb24b419ceb928bb7133cbc353
Coder-the-noob <moinmatubbar6@gmail.com>
2025-12-23 00:32:49 +0600

    fix some bug

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ function verifyJWT(req, res, next) {
 
 async function run() {
   try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
+    // await client.connect();
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
